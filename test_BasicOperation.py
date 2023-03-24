40e5dae7a1e8b75409fef1a3cc881210d1cd36ec
user-poc <soundarapandiyan.ravichandran@in.verizon.com>
2023-03-24 11:35:28 +0530

    added logs

--- test_BasicOperation.py
+++ test_BasicOperation.py
@@ -1,4 +1,4 @@
-import pytest
+import logging
 import allure
 
 
@@ -6,14 +6,17 @@ import allure
 class TestBasicOperation:
     def test_add(self):
         print("Function to do addition")
+        logging.info("logging info - Function to do addition")
         assert 4 == 3, "4 is not equal to 3"
 
     def test_sub(self):
         print("Function to do subtraction")
+        logging.warning("logging warning - Function to do addition")
         assert True
 
     def test_mul(self):
         print("Function to do multiply")
+        logging.error("logging error - Function to do addition")
         assert 'a'!='b'
 
     def test_div(self):
