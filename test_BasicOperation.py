fbc61de42979c9910e3be9f946d317f4e77d6181
user-poc <soundarapandiyan.ravichandran@in.verizon.com>
2023-03-24 11:58:52 +0530

    added log level

--- test_BasicOperation.py
+++ test_BasicOperation.py
@@ -4,6 +4,7 @@ import allure
 
 @allure.suite("TestBasicOperation")
 class TestBasicOperation:
+    logging.basicConfig(level=logging.DEBUG)
     def test_add(self):
         print("Function to do addition")
         logging.info("logging info - Function to do addition")
@@ -11,18 +12,20 @@ class TestBasicOperation:
 
     def test_sub(self):
         print("Function to do subtraction")
-        logging.warning("logging warning - Function to do addition")
+        logging.warning("logging warning - Function to do subtraction")
         assert True
 
     def test_mul(self):
         print("Function to do multiply")
-        logging.error("logging error - Function to do addition")
+        logging.error("logging error - Function to do multiply")
         assert 'a'!='b'
 
     def test_div(self):
         print("Function to do division")
+        logging.info("logging info - Function to do division")
         assert 'a' != 'a', "a is not as expected"
 
     def test_mod(self):
         print("Function to do mod")
+        logging.info("logging info - Function to do mod")
         assert 'a' == 'a', "a is not as expected"
